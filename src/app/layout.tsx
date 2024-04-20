095634c5a61256edf3617c4e194bfe33c6195b26
SaifAlqady51 <saifalqady51@gmail.com>
2024-04-20 15:14:47 +0200

    ui: Added Navbar component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from '@/components/Header'
-import Providers from './Providers'
+import Header from "@/components/Header";
+import Providers from "./Providers";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
           <Header />
+          <Navbar />
           {children}
         </Providers>
       </body>
