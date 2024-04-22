980fc2c6526501dd51b65139b3f64ce76beaaa1b
SaifAlqady51 <saifalqady51@gmail.com>
2024-04-22 17:45:47 +0200

    feat: Handled fetching errors resolves #13

---
+++ src/app/error.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useEffect } from "react";
+
+type ErrorTypeProps = {
+  error: string;
+  reset: any;
+};
+
+export default function Error({ error, reset }: ErrorTypeProps) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+  return (
+    <div className=" text-center mt-10">
+      <h1>Something went wrong, Please try again later.</h1>
+      <button className="hover:text-amber-600" onClick={() => reset()}>
+        Try Again
+      </button>
+    </div>
+  );
+}
