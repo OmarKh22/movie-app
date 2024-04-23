e538b8a75274772fb239557882813aef1e3b322c
SaifAlqady51 <saifalqady51@gmail.com>
2024-04-23 09:39:31 +0200

    ui: Created Card component resolves #15

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
+import Card from "./Card";
 
 type ResultsType = {
-    results : any
-    res : any 
-}
+  results: any;
+  res: any;
+};
 
-export default function Results({ results } : ResultsType ) {
+export default function Results({ results }: ResultsType) {
   return (
-    <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4'>
-      {results.map((res : any)  => (
-        <h1>{res.original_title}</h1>
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-[1500px] mx-auto py-4">
+      {results.map((res: any) => (
+        <Card key={res.id} result={res} />
       ))}
     </div>
   );
