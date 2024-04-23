2106bced3e1f5f3c6d00f67d9f4376422974e9fa
OmarKh22 <ok597451@gmail.com>
2024-04-23 14:38:52 +0200

    editing some colors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import NavbarItem from "./NavbarItem";
 export default function Navbar() {
   return (
     <Suspense>
-      <div className="flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-xl justify-center gap-12">
+      <div className="flex dark:bg-[#191919] bg-amber-100 p-4 lg:text-xl justify-center gap-12">
         <NavbarItem title="Trending" param="fetchTrending" />
         <NavbarItem title="Top Rated" param="fetchTopRated" />
       </div>
