805bfecc39d4db9e1aa56fdaa2e5f8919934a302
SaifAlqady51 <saifalqady51@gmail.com>
2024-04-23 14:35:14 +0200

    ui: Created search Page

---
+++ src/app/search/[searchTerm]/page.tsx
@@ -0,0 +1,27 @@
+import Results from "@/components/Results";
+
+const API_KEY = process.env.API_KEY;
+
+type SearchParamsType = {
+  searchParams: {
+    searchTerm: string;
+  };
+};
+export default async function SearchPage({ searchParams }: SearchParamsType) {
+  const searchTerm = searchParams.searchTerm;
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`
+  );
+  const data = await res.json();
+  const results = data.results;
+
+  return (
+    <div>
+      {results &&
+        results.length ===
+        <h1 className="text-center pt-6">No results found</h1>}
+      {results && <Results results={results} />}
+    </div>
+  );
+}
