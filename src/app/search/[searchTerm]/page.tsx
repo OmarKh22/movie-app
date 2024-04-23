fa2131bb2daa2c2017b6e9c68c467c299cb4f482
SaifAlqady51 <saifalqady51@gmail.com>
2024-04-23 17:49:28 +0200

    refactor: fixed search page results

--- src/app/search/[searchTerm]/page.tsx
+++ src/app/search/[searchTerm]/page.tsx
@@ -1,21 +1,12 @@
 import Results from "@/components/Results";
 
-const API_KEY = process.env.API_KEY;
-
-type SearchParamsType = {
-  searchParams: {
-    searchTerm: string;
-  };
-};
-export default async function SearchPage({ searchParams }: SearchParamsType) {
-  const searchTerm = searchParams.searchTerm;
-
+export default async function SearchPage({ params }: { params: any }) {
+  const seachTerm = params.searchTerm;
   const res = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${seachTerm}&language=en-US&page=1&include_adult=false`
   );
   const data = await res.json();
   const results = data.results;
-
   return (
     <div>
       {results &&
